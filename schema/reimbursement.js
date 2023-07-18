80d4d003092fb0033583862ee23a99a2d6505bf3
Ananth <110927979+Ananththandavarayamurthi@users.noreply.github.com>
2023-07-18 12:11:22 +0530

    Update reimbursement.js

--- schema/reimbursement.js
+++ schema/reimbursement.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const reimbursementSchema = new mongoose.Schema({
   typeOfExpense: { type: String, required: true },
   date: { type: Date, required: true },
-  amount: { type: Number, required: true },
+  amount: { type: String, required: true },
   status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
   // Add other relevant fields for the reimbursement
 });
